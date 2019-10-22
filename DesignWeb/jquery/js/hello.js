84cb90721ba043898b676ddd4ed5de5e7d91843b
Souleymane Soumaré <soumare.souleymane@carrefour.cegepvicto.ca>
2019-10-22 08:34:17 -0400

    ajout

---
+++ DesignWeb/jquery/js/hello.js
@@ -0,0 +1,7 @@
+
+    $('#stormBlanc').attr("src","images/nouveauStorm.jpeg")
+    $('#stormBlanc').attr("title","Pas facile la vie de Stormtrooper")
+    $('#stormBlanc').attr("alt","image d'un stormtrooper blanc")
+    $('#fontAChanger').attr("class","fas fa-fighter-jet fa-4x")
+    $('#zama').css("font-size","4rem")
+
