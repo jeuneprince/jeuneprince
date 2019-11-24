a2cf9d8ba07e3ce11aedc76dd301e4d246f4e750
Souleymane Soumaré <soumare.souleymane@carrefour.cegepvicto.ca>
2019-11-23 20:36:20 -0500

    ajout

--- DesignWeb/ExamD2/js/monjavascript.js
+++ DesignWeb/ExamD2/js/monjavascript.js
@@ -237,181 +237,182 @@ $(document).on('keyup',function(e) {
     var grosseur=mot.length;
     var i;
     var j;
+    if(e.which == 13) {
+        if (grosseur===3){
 
-    if (grosseur===3){
+            $('#row1').hide();
+            $('#row2').hide();
+            $('#row3').hide();
+            $('#row4').show();
+            $('#row5').hide();
+            for (i=0;i<3;i++){
+                j=i+1;
+                if (mot[i]==='*'){
+                    $("#col4n"+j).attr("src","Letters/CS/CS1.jpg");
+                }
+                else {
+                    $("#col4n"+j).attr("src","Letters/"+mot[i]+"/"+mot[i]+"1.jpg");
+                }
+            }
+        }
+        else if (grosseur===4){
+            $('#row1').hide();
+            $('#row2').hide();
+            $('#row3').show();
+            $('#row4').hide();
+            $('#row5').hide();
+            for (i=0;i<4;i++){
+                j=i+1;
+                if (mot[i]==='*'){
+                    $("#col3n"+j).attr("src","Letters/CS/CS1.jpg");
+                }
+                else {
+                    $("#col3n"+j).attr("src","Letters/"+mot[i]+"/"+mot[i]+"1.jpg");
+                }
+            }
 
-        $('#row1').hide();
-        $('#row2').hide();
-        $('#row3').hide();
-        $('#row4').show();
-        $('#row5').hide();
-        for (i=0;i<3;i++){
-            j=i+1;
-            if (mot[i]==='*'){
-                $("#col4n"+j).attr("src","Letters/CS/CS1.jpg");
-            }
-            else {
-                $("#col4n"+j).attr("src","Letters/"+mot[i]+"/"+mot[i]+"1.jpg");
+        }
+        else if (grosseur===5){
+            $('#row1').hide();
+            $('#row2').hide();
+            $('#row3').hide();
+            $('#row4').hide();
+            $('#row5').show();
+            for (i=0;i<5;i++){
+                j=i+1;
+                if (mot[i]==='*'){
+                    $("#col5n"+j).attr("src","Letters/CS/CS1.jpg");
+                }
+                else {
+                    $("#col5n"+j).attr("src","Letters/"+mot[i]+"/"+mot[i]+"1.jpg");
+                }
             }
         }
-    }
-    else if (grosseur===4){
-        $('#row1').hide();
-        $('#row2').hide();
-        $('#row3').show();
-        $('#row4').hide();
-        $('#row5').hide();
-        for (i=0;i<4;i++){
-            j=i+1;
-            if (mot[i]==='*'){
-                $("#col3n"+j).attr("src","Letters/CS/CS1.jpg");
-            }
-            else {
-                $("#col3n"+j).attr("src","Letters/"+mot[i]+"/"+mot[i]+"1.jpg");
-            }
-        }
-
-    }
-    else if (grosseur===5){
-        $('#row1').hide();
-        $('#row2').hide();
-        $('#row3').hide();
-        $('#row4').hide();
-        $('#row5').show();
-        for (i=0;i<5;i++){
-            j=i+1;
-            if (mot[i]==='*'){
-                $("#col5n"+j).attr("src","Letters/CS/CS1.jpg");
-            }
-            else {
-                $("#col5n"+j).attr("src","Letters/"+mot[i]+"/"+mot[i]+"1.jpg");
+        else if (grosseur===6){
+            $('#row1').hide();
+            $('#row2').show();
+            $('#row3').hide();
+            $('#row4').hide();
+            $('#row5').hide();
+            for (i=0;i<6;i++){
+                j=i+1;
+                if (mot[i]==='*'){
+                    $("#col2n"+j).attr("src","Letters/CS/CS1.jpg");
+                }
+                else {
+                    $("#col2n"+j).attr("src","Letters/"+mot[i]+"/"+mot[i]+"1.jpg");
+                }
             }
         }
-    }
-    else if (grosseur===6){
-        $('#row1').hide();
-        $('#row2').show();
-        $('#row3').hide();
-        $('#row4').hide();
-        $('#row5').hide();
-        for (i=0;i<6;i++){
-            j=i+1;
-            if (mot[i]==='*'){
-                $("#col2n"+j).attr("src","Letters/CS/CS1.jpg");
+        else if (grosseur===7){
+            $('#row1').show();
+            $('#row2').hide();
+            $('#row3').hide();
+            $('#row4').hide();
+            $('#row5').hide();
+            for (i=0;i<7;i++){
+                j=i+1;
+                if (mot[i]==='*'){
+                    $("#col1n"+j).attr("src","Letters/CS/CS1.jpg");
+                }
+                else {
+                    $("#col1n"+j).attr("src","Letters/"+mot[i]+"/"+mot[i]+"1.jpg");
+                }
             }
-            else {
-                $("#col2n"+j).attr("src","Letters/"+mot[i]+"/"+mot[i]+"1.jpg");
-            }
+            $("#col1n8").attr("src","");
+            $("#col1n9").attr("src","");
+            $("#col1n10").attr("src","");
+            $("#col1n11").attr("src","");
+            $("#col1n12").attr("src","");
         }
-    }
-    else if (grosseur===7){
-        $('#row1').show();
-        $('#row2').hide();
-        $('#row3').hide();
-        $('#row4').hide();
-        $('#row5').hide();
-        for (i=0;i<7;i++){
-            j=i+1;
-            if (mot[i]==='*'){
-                $("#col1n"+j).attr("src","Letters/CS/CS1.jpg");
+        else if (grosseur===8){
+            $('#row1').show();
+            $('#row2').hide();
+            $('#row3').hide();
+            $('#row4').hide();
+            $('#row5').hide();
+            for (i=0;i<8;i++){
+                j=i+1;
+                if (mot[i]==='*'){
+                    $("#col1n"+j).attr("src","Letters/CS/CS1.jpg");
+                }
+                else {
+                    $("#col1n"+j).attr("src","Letters/"+mot[i]+"/"+mot[i]+"1.jpg");
+                }
             }
-            else {
-                $("#col1n"+j).attr("src","Letters/"+mot[i]+"/"+mot[i]+"1.jpg");
-            }
+            $("#col1n9").attr("src","");
+            $("#col1n10").attr("src","");
+            $("#col1n11").attr("src","");
+            $("#col1n12").attr("src","");
         }
-        $("#col1n8").attr("src","");
-        $("#col1n9").attr("src","");
-        $("#col1n10").attr("src","");
-        $("#col1n11").attr("src","");
-        $("#col1n12").attr("src","");
-    }
-    else if (grosseur===8){
-        $('#row1').show();
-        $('#row2').hide();
-        $('#row3').hide();
-        $('#row4').hide();
-        $('#row5').hide();
-        for (i=0;i<8;i++){
-            j=i+1;
-            if (mot[i]==='*'){
-                $("#col1n"+j).attr("src","Letters/CS/CS1.jpg");
+        else if (grosseur===9){
+            $('#row1').show();
+            $('#row2').hide();
+            $('#row3').hide();
+            $('#row4').hide();
+            $('#row5').hide();
+            for (i=0;i<9;i++){
+                j=i+1;
+                if (mot[i]==='*'){
+                    $("#col1n"+j).attr("src","Letters/CS/CS1.jpg");
+                }
+                else {
+                    $("#col1n"+j).attr("src","Letters/"+mot[i]+"/"+mot[i]+"1.jpg");
+                }
             }
-            else {
-                $("#col1n"+j).attr("src","Letters/"+mot[i]+"/"+mot[i]+"1.jpg");
-            }
+            $("#col1n10").attr("src","");
+            $("#col1n11").attr("src","");
+            $("#col1n12").attr("src","");
         }
-        $("#col1n9").attr("src","");
-        $("#col1n10").attr("src","");
-        $("#col1n11").attr("src","");
-        $("#col1n12").attr("src","");
-    }
-    else if (grosseur===9){
-        $('#row1').show();
-        $('#row2').hide();
-        $('#row3').hide();
-        $('#row4').hide();
-        $('#row5').hide();
-        for (i=0;i<9;i++){
-            j=i+1;
-            if (mot[i]==='*'){
-                $("#col1n"+j).attr("src","Letters/CS/CS1.jpg");
+        else if (grosseur===10){
+            $('#row1').show();
+            $('#row2').hide();
+            $('#row3').hide();
+            $('#row4').hide();
+            $('#row5').hide();
+            for (i=0;i<10;i++){
+                j=i+1;
+                if (mot[i]==='*'){
+                    $("#col1n"+j).attr("src","Letters/CS/CS1.jpg");
+                }
+                else {
+                    $("#col1n"+j).attr("src","Letters/"+mot[i]+"/"+mot[i]+"1.jpg");
+                }
             }
-            else {
-                $("#col1n"+j).attr("src","Letters/"+mot[i]+"/"+mot[i]+"1.jpg");
-            }
+            $("#col1n11").attr("src","");
+            $("#col1n12").attr("src","");
         }
-        $("#col1n10").attr("src","");
-        $("#col1n11").attr("src","");
-        $("#col1n12").attr("src","");
-    }
-    else if (grosseur===10){
-        $('#row1').show();
-        $('#row2').hide();
-        $('#row3').hide();
-        $('#row4').hide();
-        $('#row5').hide();
-        for (i=0;i<10;i++){
-            j=i+1;
-            if (mot[i]==='*'){
-                $("#col1n"+j).attr("src","Letters/CS/CS1.jpg");
+        else if (grosseur===11){
+            $('#row1').show();
+            $('#row2').hide();
+            $('#row3').hide();
+            $('#row4').hide();
+            $('#row5').hide();
+            for (i=0;i<11;i++){
+                j=i+1;
+                if (mot[i]==='*'){
+                    $("#col1n"+j).attr("src","Letters/CS/CS1.jpg");
+                }
+                else {
+                    $("#col1n"+j).attr("src","Letters/"+mot[i]+"/"+mot[i]+"1.jpg");
+                }
             }
-            else {
-                $("#col1n"+j).attr("src","Letters/"+mot[i]+"/"+mot[i]+"1.jpg");
-            }
+            $("#col1n12").attr("src","");
         }
-        $("#col1n11").attr("src","");
-        $("#col1n12").attr("src","");
-    }
-    else if (grosseur===11){
-        $('#row1').show();
-        $('#row2').hide();
-        $('#row3').hide();
-        $('#row4').hide();
-        $('#row5').hide();
-        for (i=0;i<11;i++){
-            j=i+1;
-            if (mot[i]==='*'){
-                $("#col1n"+j).attr("src","Letters/CS/CS1.jpg");
-            }
-            else {
-                $("#col1n"+j).attr("src","Letters/"+mot[i]+"/"+mot[i]+"1.jpg");
-            }
-        }
-        $("#col1n12").attr("src","");
-    }
-    else if (grosseur===12){
-        $('#row1').show();
-        $('#row2').hide();
-        $('#row3').hide();
-        $('#row4').hide();
-        $('#row5').hide();
-        for (i=0;i<12;i++){
-            j=i+1;
-            if (mot[i]==='*'){
-                $("#col1n"+j).attr("src","Letters/CS/CS1.jpg");
-            }
-            else {
-                $("#col1n"+j).attr("src","Letters/"+mot[i]+"/"+mot[i]+"1.jpg");
+        else if (grosseur===12){
+            $('#row1').show();
+            $('#row2').hide();
+            $('#row3').hide();
+            $('#row4').hide();
+            $('#row5').hide();
+            for (i=0;i<12;i++){
+                j=i+1;
+                if (mot[i]==='*'){
+                    $("#col1n"+j).attr("src","Letters/CS/CS1.jpg");
+                }
+                else {
+                    $("#col1n"+j).attr("src","Letters/"+mot[i]+"/"+mot[i]+"1.jpg");
+                }
             }
         }
     }
