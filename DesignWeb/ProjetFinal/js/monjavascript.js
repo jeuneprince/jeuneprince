10410047babd71fe4757cd42c0fe2657c8b12576
Souleymane Soumaré <soumare.souleymane@carrefour.cegepvicto.ca>
2019-12-09 17:38:29 -0500

    ajout

--- DesignWeb/ProjetFinal/js/monjavascript.js
+++ DesignWeb/ProjetFinal/js/monjavascript.js
@@ -9,3 +9,53 @@ $(document).ready(function() {
         }, 1000);
     });
 });
+$( "#image1" ).mouseenter(function() {
+    $('#image1').css("transform","scale(1.2)");
+    $('#image1').css("transition","all 0.5s ease");
+
+
+});
+$('#image1').mouseleave(function(){
+    $('#image1').css("transform","scale(1)");
+});
+
+
+$( "#image2" ).mouseenter(function() {
+    $('#image2').css("transform","scale(1.2)");
+    $('#imag2').css("transition","all 0.5s ease");
+
+
+});
+$('#image2').mouseleave(function(){
+    $('#image2').css("transform","scale(1)");
+});
+
+
+$( "#image3" ).mouseenter(function() {
+    $('#image3').css("transform","scale(1.2)");
+    $('#image3').css("transition","all 0.5s ease");
+
+
+});
+$('#image3').mouseleave(function(){
+    $('#image3').css("transform","scale(1)");
+});
+
+$("#Email1").keyup(function(){
+    var courriel=$("#Email1").val();
+    if (courriel.includes("@" && ".")){
+        $("#small").html("Bonne adresse");
+        $("#small").css("color","green");
+    }
+    else {
+        $("#small").html("mauvaise entrée");
+        $("#small").css("color","red");
+    }
+});
+
+$("#envoyer" ).click(function() {
+    var texte_prenom=$("#prenom").val();
+    var texte_nom=$("#nom").val();
+    alert("Bonjour "+texte_prenom+" "+texte_nom+ " , nous avons bien reçu votre message");
+
+});
